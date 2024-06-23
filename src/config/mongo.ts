f713136768a162b1ec2fc78b5eef6ad4e46b3044
Matthias Mayrock <matthias.mayrock@globalchanger.com>
2024-06-23 16:12:43 +0200

    Allow creating mongo connection

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -1,5 +1,13 @@
+import mongoose from "mongoose";
+
 export const throwIfMissingMongoConfig = () => {
   if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI not found");
   }
+};
+
+export const connectMongo = async () => {
+  console.log(`Connecting to MongoDb '${process.env.MONGO_URI}'...`);
+  await mongoose.connect(process.env.MONGO_URI!);
+  console.log("Connected to MongoDb!");
 };
